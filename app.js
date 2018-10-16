daccf8bdaf59adced1613766b40add4bd8e1f608
MRJUANGAVIRIA <juanga_11@hotmail.com>
2018-10-16 17:48:24 -0500

    The keyword let

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+// CHAPTER I
 // Two ways to create function
 var createGreeting = function(message, name){
   return message + name;
@@ -19,3 +20,37 @@ var deliveryBoy = {
   }
 }
 deliveryBoy.receive();
+
+// CHAPTER II Scopes var and keyword let
+// message is overwritten
+var message = "Hello";
+{
+  message = "Hi";
+}
+console.log(message);
+
+// Using let Messages isn't overwritten, and messages are differents
+let message = "Hello";
+
+{
+  let message = "Hi";
+}
+
+console.log(message);
+
+// Other example
+var fs = [];
+
+for (let i = 0; i < 10; i++){
+  fs.push(function (){
+    console.log(i);
+  })
+}
+
+fs.forEach(function(f) {
+  f();
+})
+
+
+
+
